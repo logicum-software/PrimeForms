f5d6120d54f6203a658b0c2d0ed730c1be286461
Markus Gärtner <gaertner.m@live.de>
2021-06-25 15:58:43 +0200

    20210625 15:58

--- PrimeForms/Form1.Designer.cs
+++ PrimeForms/Form1.Designer.cs
@@ -46,7 +46,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Berechnen";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
             // 
             // progressBar1
             // 
--- PrimeForms/Form1.cs
+++ PrimeForms/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Windows.Forms;
+using System.Threading.Tasks;
 
 namespace PrimeForms
 {
@@ -11,21 +12,27 @@ namespace PrimeForms
     {
         private DateTime dtStart, dtEnd;
         private int nPrimes;
-        private static IEnumerable<int> dividents = Enumerable.Range(3, 100000);
-        private static IEnumerable<int> divisors = Enumerable.Range(2, 50000);
-        
+        private static IEnumerable<int> dividents = Enumerable.Range(3, 1000000);
+        private static IEnumerable<int> divisors = Enumerable.Range(2, 500000);
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_ClickAsync(object sender, EventArgs e)
         {
-            if (backgroundWorker1.IsBusy != true)
+            if (backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.CancelAsync();
+                button1.Text = "Berechnung starten";
+            }
+            else
             {
                 // Start the asynchronous operation.
                 backgroundWorker1.RunWorkerAsync();
                 dtStart = DateTime.Now;
+                button1.Text = "Abbrechen";
             }
         }
 
@@ -34,32 +41,29 @@ namespace PrimeForms
             BackgroundWorker worker = sender as BackgroundWorker;
             nPrimes = 1;
 
-            if (worker.CancellationPending == true)
+            foreach (int current in dividents)
             {
-                e.Cancel = true;
-                return;
-            }
-            else
-            {
-                foreach (int current in dividents)
+                if (worker.CancellationPending == true)
                 {
-                    worker.ReportProgress((current + 1) / 1000);
-                    foreach (int divisor in divisors)
+                    e.Cancel = true;
+                    return;
+                }
+                worker.ReportProgress((current + 1) / 10000);
+                foreach (int divisor in divisors)
+                {
+                    if (divisor <= current / 2)
                     {
-                        if (divisor <= current / 2)
-                        {
-                            if ((float)current % divisor == 0)
-                                break;
-                        }
-                        else
-                        {
-                            nPrimes++;
+                        if ((float)current % divisor == 0)
                             break;
-                        }
+                    }
+                    else
+                    {
+                        nPrimes++;
+                        break;
                     }
                 }
-                e.Result = nPrimes;
             }
+            e.Result = nPrimes;
         }
 
         private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
